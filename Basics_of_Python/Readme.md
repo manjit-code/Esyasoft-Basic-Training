828952e62e878e3c345858907f22666da2d9c64c
manjit-code <manjit.majhi@esyasof.com>
2025-09-04 14:11:53 +0530

    Python code added

---
+++ Basics_of_Python/Readme.md
@@ -0,0 +1,2 @@
+## 03/09/2025
+Assignment-1: Calculating Area of a Triangle problem solution added.
---
+++ Basics_of_Python/main.py
@@ -0,0 +1,8 @@
+# Assignment-1: Calculating Area of a Triangle 
+
+base = int(input("Enter the base of the triange: "))
+height = int(input("Enter the height of the triange: "))
+
+area = base * height * 0.5
+
+print(f'Area of triangle is {area}')

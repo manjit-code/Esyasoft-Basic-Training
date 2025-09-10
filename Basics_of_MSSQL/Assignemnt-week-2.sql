d2aeb6ea7a7c8db39e45c319e723922f18e644ca
manjit-code <manjit.majhi@esyasof.com>
2025-09-10 15:49:39 +0530

    Completed SQL Assignment week 2

---
+++ Basics_of_MSSQL/Assignemnt-week-2.sql
@@ -0,0 +1,96 @@
+USE manjit;
+
+-- users table--
+CREATE TABLE users(user_id INT PRIMARY KEY, email VARCHAR(20) UNIQUE, name VARCHAR(50));
+
+SELECT * FROM users;
+
+--books table--
+CREATE TABLE books(product_id INT PRIMARY KEY, title VARCHAR(50), price FLOAT);
+
+SELECT * FROM books;
+
+-- orders--
+CREATE TABLE orders(order_no INT PRIMARY KEY, user_id INT, product_id INT, FOREIGN KEY (user_id) REFERENCES users (user_id), 
+FOREIGN KEY (product_id) REFERENCES books (product_id));
+
+SELECT * FROM orders;
+
+ALTER TABLE users ALTER COLUMN email VARCHAR(50);
+
+-- users table data insert --
+INSERT INTO users(user_id, email, name) VALUES
+(1, 'callmebhai@gmail.com', 'Gopal'),
+(2, 'toxicmanji@gmail.com', 'Manjit'),
+(3, 'kalajadu@gmail.com', 'Soumyadeep'),
+(4, 'vimalpanmasala@gmail.com', 'JaiSai'),
+(5, 'ghostlaugh@gmail.com', 'Namitha');
+
+-- books table data insert --
+INSERT INTO books VALUES
+(101, 'MSSQL', 100),
+(102, 'Half Girlfriend', 699),
+(103, 'Black Magic',700),
+(104, 'How to Marfa', 2000),
+(105, 'How to Laugh', 2001.99);
+
+-- orders table data insert --
+INSERT INTO orders VALUES
+(401, 1, 101), (402, 2,  102), (403, 2, 103), (404, 3, 104), (405, 3, 103), (406, 4, 102), (407, 5, 102), (408, 5, 105);
+
+
+-- Show books purchased by each user--
+-- users.user_id, users.name, books.title, orders.order_id---
+
+SELECT u.user_id, u.name, b.title, o.order_no FROM users u INNER JOIN orders o ON u.user_id = o.user_id
+INNER JOIN books b ON o.product_id = b.product_id;
+
+SELECT * FROM users u INNER JOIN orders o ON u.user_id = o.user_id
+INNER JOIN books b ON o.product_id = b.product_id;
+
+-- Find who have purchased 'Black Magic' Book
+SELECT * FROM users u INNER JOIN orders o ON u.user_id = o.user_id
+INNER JOIN books b ON o.product_id = b.product_id 
+WHERE o.product_id = (SELECT product_id FROM books WHERE title = 'Black Magic');
+
+--- Find most expencive book---
+SELECT * FROM users u INNER JOIN orders o ON u.user_id = o.user_id
+INNER JOIN books b ON o.product_id = b.product_id WHERE b.price = (SELECT MAX(price) FROM books);
+
+
+
+--- Task-2 ---
+-- 1 --
+SELECT * FROM users u INNER JOIN orders o ON u.user_id = o.user_id 
+INNER JOIN books b ON o.product_id = b.product_id;
+
+--2--
+SELECT COUNT(*) FROM orders;
+
+--3--
+SELECT * FROM users u INNER JOIN orders o ON u.user_id = o.user_id 
+INNER JOIN books b ON o.product_id = b.product_id WHERE o.product_id IN (102, 104);
+
+
+--4--
+select u.user_id, u.name, COUNT(*) FROM orders o INNER JOIN users u ON u.user_id=o.user_id GROUP BY u.user_id, u.name;
+
+--5--
+SELECT u.user_id, u.name, SUM(b.price) as total_spend FROM books b INNER JOIN orders o ON o.product_id = b.product_id
+INNER JOIN users u ON u.user_id = o.user_id GROUP BY u.user_id, u.name;
+
+--6--
+SELECT TOP 1 u.user_id, u.name, SUM(b.price) as total_spend FROM books b INNER JOIN orders o ON o.product_id = b.product_id
+INNER JOIN users u ON u.user_id = o.user_id GROUP BY u.user_id, u.name ORDER BY total_spend DESC;
+
+--7--
+SELECT * FROM books ORDER BY price DESC;
+
+--8--
+SELECT TOP 1 * FROM books ORDER BY price DESC;
+
+--9--
+SELECT TOP 1 * FROM books ORDER BY price ASC;
+
+--10--
+SELECT TOP 1 * FROM books ORDER BY product_id DESC;
